e25f98ad3c4ba3b298eff8db53788fb083e6d946
NathanVillamor <sfnathanvillamor@gmail.com>
2024-05-20 19:07:20 +0800

    Commit #6
    Progress:
    - Finished the Javascript email & password input validation for both signup & login
    - Created arrays as temporary databases for email and passwords

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // VARIABLES
 
+var signupFormContainer = document.querySelector(".signup-container");
+var loginFormContainer = document.querySelector(".login-container");
+var mainHero = document.querySelector(".main-hero");
+
 // Main Hero 
 var username = document.querySelector(".username");
 var heroname = document.querySelector(".heroName");
@@ -7,29 +11,33 @@ var heroBtn1 = document.getElementById("cta1");
 var heroBtn2 = document.getElementById("cta2");
 
 // Sign-up Form
-var signupEmail = document.getElementById("signupEmail").value;
-var signupPassword = document.getElementById("signupPassword").value;
+var signupEmail = document.getElementById("signupEmail");
+var signupPassword = document.getElementById("signupPassword");
 var termsCheck = document.getElementById("termsCheck");
 var signupBtn = document.getElementById("signupBtn");
 
 // Login Form
-var loginEmail = document.getElementById("loginEmail").value;
-var loginPassword = document.getElementById("loginPassword").value;
+var loginEmail = document.getElementById("loginEmail");
+var loginPassword = document.getElementById("loginPassword");
 var passCheck = document.getElementById("passCheck");
 var loginBtn = document.getElementById("loginBtn");
 
+// Temporary Database of Accounts and Passwords
 var accountEmails = new Array();
 var accountPasswords = new Array();
 
-signupBtn.onclick() = function() {
+// Signup Form-Validation
+signupBtn.onclick = function() {
 
-    if (signupEmail != null || signupPassword != null) {
+    if (signupEmail != "" && signupPassword != "") {
 
-        if (signupEmail.search(/@/i)) {
+        const emailChar = /@./;
 
-            accountEmails.push(signupEmail);
-            accountPasswords.push(signupPassword);
+        if (emailChar.test(signupEmail.value)) {
 
+            accountEmails.push(signupEmail.value);
+            accountPasswords.push(signupPassword.value);
+        
             alert("Account created successfully!");
 
         } 
@@ -39,7 +47,23 @@ signupBtn.onclick() = function() {
 
     } 
     else {
-        alert("Please fill the necessary fields!")
+
+        alert("Please fill the necessary fields!");
+
+    }
+
+}
+
+// LOGIN 
+loginBtn.onclick = function() {
+
+    if (accountEmails.includes(loginEmail.value) && accountPasswords.includes(loginPassword.value)) {
+        alert("Login Successful!");
+        loginFormContainer.hide();
+        mainHero.show(500);
+    }
+    else {
+        alert("Wrong details. Please try again.")
     }
 
 }
